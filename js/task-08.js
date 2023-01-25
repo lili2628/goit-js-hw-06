812a53fabe2dff70b3a7f6149ed1af0e1bd3012e
Liliia Paliichuk <113315152+lili2628@users.noreply.github.com>
2023-01-25 01:13:52 +0100

    added HW 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  console.log(`email: ${email.value}, password: ${password.value}`);
+  event.currentTarget.reset();
+
+}
